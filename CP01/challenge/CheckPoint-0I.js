5b7d9b11a8af54223bd097eabc4db8d79af24163
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2023-09-26 18:27:45 -0300

    checkpoint 1 resuelto

--- CP01/challenge/CheckPoint-0I.js
+++ CP01/challenge/CheckPoint-0I.js
@@ -1,42 +1,51 @@
 // Ejercicio 1: Declarar una variable "nombre" y asignarle tu nombre.
 
 // Escribí tu código acá abajo:
-
+const nombre = "Nacho";
+console.log(nombre)
 
 // Ejercicio 2: Declarar una variable "edad" y asignarle tu edad.
 
 // Escribí tu código acá abajo:
-
+const edad = 41;
+console.log(edad)
 
 // Ejercicio 3: Declarar una variable "esEstudiante" y asignarle un valor booleano.
 
 // Escribí tu código acá abajo:
+const esEstudiante = true;
+console.log(esEstudiante)
 
 
 // Ejercicio 4: Declarar una variable "num1" y asignarle un número decimal.
 
 // Escribí tu código acá abajo:
-
+const num1 = 234.1;
+console.log(num1)
 
 // Ejercicio 5: Declarar una variable "num2" y asignarle un número entero.
 
 // Escribí tu código acá abajo:
-
+const num2 = 234;
+console.log(num2)
 
 // Ejercicio 6: Declarar una variable "mensaje" y asignarle un mensaje de texto.
 
 // Escribí tu código acá abajo:
-
+const mensaje = "hola mundo";
+console.log(mensaje)
 
 // Ejercicio 7: Declarar una variable "valorNulo" y asignarle el valor null.
 
 // Escribí tu código acá abajo:
-
+const valorNulo = null;
+console.log(valorNulo)
 
 // Ejercicio 8: Declarar una variable "numeroIndefinido" sin asignarle valor.
 
 // Escribí tu código acá abajo:
-
+const numeroIndefinido = undefined;
+console.log(numeroIndefinido)
 
 // Ejercicio 9: Declarar una variable "frutas" y asignarle un arreglo de nombres de frutas.
 
